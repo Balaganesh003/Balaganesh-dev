f0e6fd0c44e2335192dfa08353313155af84ecff
Balaganesh003 <k.balaganesh26@gmail.com>
2023-01-30 00:05:13 +0530

    updated the font styles in the header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,17 +12,17 @@ const icons = [FaWhatsapp, FaInstagram, FaLinkedin, FaTwitter, FaGithub];
 
 const Header = () => {
   return (
-    <div className="md:mx-6  lg:mt-6">
+    <div className="md:mx-10  md:mt-12 lg:mt-6">
       <h1 className="md:tracking-1 tracking-widest py-2 text-[0.75rem] md:text-xs  text-HeadingGray uppercase">
         Hi, My name is
       </h1>
-      <h1 className="xl:text-7xl mt-2 lg:6xl md:text-5xl text-3xl font-bold ">
+      <h1 className="xl:text-7xl mt-2 lg:6xl md:text-[2.9rem] text-3xl font-bold ">
         Balaganesh K
       </h1>
       <p className="font-extrabold  text-transparent text-[2.5rem] md:text-[3.5rem] lg:text[4.7rem] xl:text-[5rem] bg-clip-text bg-gradient-to-r from-HeadingBlue to-headingPurple  leading-tight">
         I am a Frontend Developer
       </p>
-      <p className="text-base md:text-lg pr-1 lg:text-lg max-w-[33rem] text-HeadingGray font-medium mt-6">
+      <p className="text-base md:text-lg pr-1 lg:text-lg md:max-w-md lg:max-w-[33rem] text-HeadingGray font-medium mt-6">
         I am a Frontend Developer and Python Programmer. Constantly seeking new
         challenges and opportunities to enhance my skills. I am passionate about
         technology and always strive to stay updated with the latest industry
