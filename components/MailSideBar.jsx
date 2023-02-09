28c442b47ebdeb758bd8eebac96c8dfdf0ebd8ad
Balaganesh003 <k.balaganesh26@gmail.com>
2023-02-09 21:45:49 +0530

    updated the mailsidebarrow

--- components/MailSideBar.jsx
+++ components/MailSideBar.jsx
@@ -10,8 +10,7 @@ const MailSideBar = () => {
       <div className="rotate-90 mb-20 w-10 py-[2.9rem] ">
         <a
           href="mailto:k.balaganesh26@gmail.com"
-          target="_blank"
-          rel="noreferrer"
+          target="_new"
           className="tracking-2 inline-block hover:-translate-x-1 transition-all  duration-200 cursor-pointer hover:text-secondary">
           k.balaganesh26@gmail.com
         </a>
