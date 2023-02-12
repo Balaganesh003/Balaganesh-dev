d12a84b4f991fa00ac482c561712feee91834923
Balaganesh003 <k.balaganesh26@gmail.com>
2023-02-12 23:10:09 +0530

    deleted the skillsData file

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,68 @@
 import React from 'react';
 import SkillItem from './SkillItem';
-import SkillsData from '../skillsData';
+
+const SkillsData = [
+  {
+    skillName: 'HTML',
+    skillStrength: 'before:w-[95%]',
+    percentage: 95,
+  },
+  {
+    skillName: 'CSS',
+    percentage: 75,
+    skillStrength: 'before:w-[75%]',
+  },
+  {
+    skillName: 'Javascript',
+    skillStrength: 'before:w-[75%]',
+    percentage: 75,
+  },
+  {
+    skillName: 'React Js',
+    percentage: 65,
+    skillStrength: 'before:w-[65%]',
+  },
+  {
+    skillName: 'Next Js',
+    percentage: 55,
+    skillStrength: 'before:w-[55%]',
+  },
+  {
+    skillName: 'Tailwind CSS',
+    percentage: 60,
+    skillStrength: 'before:w-[60%]',
+  },
+  {
+    skillName: 'Bootstrap',
+    percentage: 35,
+    skillStrength: 'before:w-[35%]',
+  },
+  {
+    skillName: 'Firebase',
+    percentage: 40,
+    skillStrength: 'before:w-[40%]',
+  },
+  {
+    skillName: 'Sanity',
+    percentage: 35,
+    skillStrength: 'before:w-[35%]',
+  },
+  {
+    skillName: 'Python',
+    percentage: 65,
+    skillStrength: 'before:w-[65%]',
+  },
+  {
+    skillName: 'C',
+    percentage: 60,
+    skillStrength: 'before:w-[60%]',
+  },
+  {
+    skillName: 'C++',
+    percentage: 55,
+    skillStrength: 'before:w-[55%]',
+  },
+];
 
 const Skills = () => {
   return (
