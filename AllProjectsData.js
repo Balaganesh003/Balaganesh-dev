418ee3feffdeb8fa66effd45a297ca64be06d716
Balaganesh003 <k.balaganesh26@gmail.com>
2023-03-05 22:31:01 +0530

    Updated the projects data

--- AllProjectsData.js
+++ AllProjectsData.js
@@ -49,13 +49,88 @@ const totalProjects = [
   },
   {
     id: 5,
-    title: 'Sanke Game',
+    title: 'Snake Game',
     category: 'python',
     description:
       'Snake Game is a simple game where the player controls a snake and tries to eat as many apples as possible. The snake grows longer with each apple it eats.',
     image: '/snake-game.jpg',
     githubLink:
       'https://github.com/Balaganesh003/python-projects/tree/main/sanke%20game%20using%20turtle',
+  },
+  {
+    id: 6,
+    title: 'Find Coffee Stores',
+    category: 'WebApp',
+    description: 'This is a coffee store finder app. ',
+    image: '/coffee-store.gif',
+    githubLink: 'https://github.com/Balaganesh003/discover-coffee-stores',
+  },
+  {
+    id: 7,
+    title: 'LinkTree Clone',
+    category: 'WebPage',
+    description:
+      'LinkTree clone is a social media application that allows users add links to their social media profiles.',
+    image: '/linktree-clone.gif',
+    githubLink: 'https://github.com/Balaganesh003/balaganesh-linktree',
+    liveLink: 'https://balaganesh-linktree.netlify.app',
+  },
+  {
+    id: 8,
+    title: 'Portfolio Website',
+    category: 'WebPage',
+    description:
+      'This is my portfolio website. It showcases my projects and skills.',
+    image: '/personal-portfolio.gif',
+    githubLink: 'https://github.com/Balaganesh003/personal-portfolio',
+    liveLink: 'https://balaganesh003.github.io/personal-portfolio/',
+  },
+  {
+    id: 9,
+    title: 'Password Generator',
+    category: 'python',
+    description:
+      'Password Generator is a simple application that generates a random password based on the users input.',
+    image: '/password-generator.jpg',
+    githubLink:
+      'https://github.com/Balaganesh003/python-projects/tree/main/password%20generator',
+  },
+  {
+    id: 10,
+    title: 'TinDog Website',
+    category: 'WebPage',
+    description:
+      'TinDog is a dating website for dogs. It is a fully responsive website that is built using HTML, CSS, and Bootstrap.',
+    image: '/tindog-website.gif',
+    githubLink: 'https://github.com/Balaganesh003/Tindog-website',
+  },
+  {
+    id: 11,
+    title: 'Netflix Clone',
+    category: 'WebApp',
+    description:
+      'Netflix clone is a movie streaming application. Use Next Auth for sign-in and sign-up',
+    image: '/netflix-clone.gif',
+    githubLink: 'https://github.com/Balaganesh003/nextjs-netflix-clone',
+  },
+  {
+    id: 12,
+    title: 'Amazon Price Tracker',
+    category: 'python',
+    description:
+      'Amazon Price Tracker is a simple application that tracks the price of a product on Amazon and sends an email to the user when the price of the product falls below a certain threshold.',
+    image: '/amazon-price-tracker.jpg',
+    githubLink:
+      'https://github.com/Balaganesh003/python-projects/tree/main/Amazon%20price%20tracker',
+  },
+  {
+    id: 13,
+    title: 'Twitter Bot',
+    category: 'python',
+    description:
+      'Twitter Bot is a simple application that automatically tweets a message if ur internet is down',
+    image: '/twitter-bot.jpg',
+    githubLink: 'https://github.com/Balaganesh003/twitter_bot',
   },
 ];
 
