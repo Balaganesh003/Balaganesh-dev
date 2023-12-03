4876ea858511ee06ca018a51e39bb449ef45af16
Balaganesh K <k.balaganesh26@gmail.com>
2023-12-03 21:27:50 +0530

    updated the resume page

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -38,8 +38,9 @@ const Navigation = () => {
   return (
     <div className="bg-primary w-full text-white">
       <div
-        className={`hidden md:flex items-center justify-between h-[6rem] bg-primary transition-transform duration-200 md:px-[3.5rem]   ${
-          isSticky && 'shadow-2xl fixed inset-x-0 top-0  z-50 '
+        className={`hidden md:flex items-center justify-between h-[5rem] bg-primary transition-transform duration-200 md:px-[3.5rem]   ${
+          isSticky &&
+          'shadow-2xl fixed inset-x-0 top-0  z-50 bg-primary/70 backdrop-blur-md'
         }`}>
         {/* Desktop */}
         <div className="cursor-pointer active:scale-90 duration-200">
@@ -70,7 +71,8 @@ const Navigation = () => {
         <div className="relative">
           <div
             className={`w-full ${
-              isSticky && 'shadow-2xl fixed inset-x-0 top-0 z-[2]'
+              isSticky &&
+              'shadow-2xl fixed inset-x-0 top-0 bg-primary/70 backdrop-blur-md z-[2]'
             } bg-primary flex justify-between h-[5rem] px-[1.5rem]`}>
             <div
               className="cursor-pointer active:scale-90 transition-all duration-200 flex items-center justify-center"
--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { useState, useEffect } from 'react';
+import DownloadPdfButton from '@/components/DownloadButton';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-const resume = () => {
-  return <div>resume</div>;
+const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center bg-primary overflow-x-hidden gap-8 py-8">
+      <Document
+        file={'./Balaganesh-resume.pdf'}
+        className={'overflow-y-hidden'}>
+        <Page
+          pageNumber={1}
+          renderMode="canvas"
+          scale={width > 786 ? 1.5 : 0.6}
+        />
+      </Document>
+    </div>
+  );
 };
 
-export default resume;
+export default Resume;
