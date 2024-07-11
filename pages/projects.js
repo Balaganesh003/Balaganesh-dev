68ac1638f54bf0832848a0e7386a0a0c0dae75c0
Balaganesh K <k.balaganesh26@gmail.com>
2024-07-11 19:17:12 +0530

    Update scrollbar width to full in DaysICode component and adjust styling in Projects component

--- pages/projects.js
+++ pages/projects.js
@@ -48,7 +48,7 @@ const Projects = ({ projectsData }) => {
         <Spinner />
       ) : (
         <div
-          className={`bg-primary min-h-screen overflow-hidden px-[1.5rem] md:px-[7rem] xl:px-[10rem] text-white ${
+          className={`bg-primary pb-5 min-h-screen overflow-hidden px-[1.5rem] md:px-[7rem] xl:px-[10rem] text-white ${
             isStickyNav && 'mt-[5rem]'
           } `}>
           <h1 className="text-4xl uppercase text-center py-5 pb-10">
