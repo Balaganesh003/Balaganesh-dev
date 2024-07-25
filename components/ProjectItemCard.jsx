22a6f785cb8416e91c35d112093a41694d555d6e
Balaganesh K <k.balaganesh26@gmail.com>
2024-07-26 01:19:39 +0530

    chore: Update ProjectItemCard component with motion animation and remove lazy loading for images

--- components/ProjectItemCard.jsx
+++ components/ProjectItemCard.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const ProjectItemCard = React.memo(
   ({ title, imageUrl, description, githubLink }) => {
     return (
-      <Link href={githubLink} target="_blank" rel="noopener noreferrer">
-        <div className="max-w-[25rem] w-full h-full transition-transform duration-300 m-auto border border-gray-700 hover:border-secondary px-4 py-6 rounded-lg hover:scale-[102%]">
-          <div className="relative w-full h-48">
-            <Image
-              src={imageUrl}
-              alt={title}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg"
-              loading="lazy"
-            />
+      <motion.div
+        initial={{ opacity: 0, y: 25, scale: 0.8 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        exit={{ opacity: 0, y: 40, scale: 0.8 }}
+        transition={{ duration: 0.75, ease: 'easeInOut' }}
+        className="rounded-lg ">
+        <Link href={githubLink} target="_blank" rel="noopener noreferrer">
+          <div className="max-w-[25rem] w-full h-full transition-transform duration-300 m-auto border border-gray-700 hover:border-secondary px-4 py-6 rounded-lg hover:scale-[102%]">
+            <div className="relative w-full h-48">
+              <Image
+                src={imageUrl}
+                alt={title}
+                width={500}
+                height={400}
+                objectFit="cover"
+                className="rounded-lg"
+                loading="lazy"
+              />
+            </div>
+            <h2 className="text-2xl font-bold text-center py-2">{title}</h2>
+            <p className="text-center text-HeadingGray">{description}</p>
           </div>
-          <h2 className="text-2xl font-bold text-center py-2">{title}</h2>
-          <p className="text-center text-HeadingGray">{description}</p>
-        </div>
-      </Link>
+        </Link>
+      </motion.div>
     );
   }
 );
